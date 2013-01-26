a3eff7c3c4bc7846dd2eede75fdd6a47352b6110
Matt Zanchelli <matt@mdznr.com>
2013-01-26 01:18:22 -0500

    Proper alignment and centering of cards and page control.

--- Passcode/PCDWalkthroughViewController.m
+++ Passcode/PCDWalkthroughViewController.m
@@ -25,31 +25,32 @@
 	cardBackground = [cardBackground resizableImageWithCapInsets:UIEdgeInsetsMake(12, 13, 14, 13)
 													resizingMode:UIImageResizingModeStretch];
 	CGRect cardRect = CGRectMake(17, 18, 286, 352);
+	CGPoint cardCenter = CGPointMake(self.view.center.x, self.view.center.y-14);
 	
 	UIImageView *cb1 = [[UIImageView alloc] initWithImage:cardBackground];
 	[cb1 setFrame:cardRect];
-	[cb1 setCenter:CGPointMake(self.view.center.x, self.view.center.y-10)];
+	[cb1 setCenter:cardCenter];
 	[cb1 setAutoresizingMask:( UIViewAutoresizingFlexibleHeight |
 							   UIViewAutoresizingFlexibleWidth )];
 	[_page1 insertSubview:cb1 atIndex:0];
 	
 	UIImageView *cb2 = [[UIImageView alloc] initWithImage:cardBackground];
 	[cb2 setFrame:cardRect];
-	[cb2 setCenter:CGPointMake(self.view.center.x, self.view.center.y-10)];
+	[cb2 setCenter:cardCenter];
 	[cb2 setAutoresizingMask:( UIViewAutoresizingFlexibleHeight |
 							   UIViewAutoresizingFlexibleWidth )];
 	[_page2 insertSubview:cb2 atIndex:0];
 	
 	UIImageView *cb3 = [[UIImageView alloc] initWithImage:cardBackground];
 	[cb3 setFrame:cardRect];
-	[cb3 setCenter:CGPointMake(self.view.center.x, self.view.center.y-10)];
+	[cb3 setCenter:cardCenter];
 	[cb3 setAutoresizingMask:( UIViewAutoresizingFlexibleHeight |
 							   UIViewAutoresizingFlexibleWidth )];
 	[_page3 insertSubview:cb3 atIndex:0];
 	
 	UIImageView *cb4 = [[UIImageView alloc] initWithImage:cardBackground];
 	[cb4 setFrame:cardRect];
-	[cb4 setCenter:CGPointMake(self.view.center.x, self.view.center.y-10)];
+	[cb4 setCenter:cardCenter];
 	[cb4 setAutoresizingMask:( UIViewAutoresizingFlexibleHeight |
 							   UIViewAutoresizingFlexibleWidth )];
 	[_page4 insertSubview:cb4 atIndex:0];
