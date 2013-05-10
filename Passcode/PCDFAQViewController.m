72edb3488e0e8cee413564d5f60f8735ce8d5d1b
Matt Zanchelli <matt@mdznr.com>
2013-05-09 20:06:57 -0400

    Current FAQs are put into place (without links). The FAQs are going to be updated/edited/reordered/removed/added.

--- Passcode/PCDFAQViewController.m
+++ Passcode/PCDFAQViewController.m
@@ -53,16 +53,29 @@
 - (void)setup
 {
 	_questionsAndAnswers = [[NSMutableArray alloc] initWithCapacity:4];
-	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"1This is a question that may span multiple lines."
-																	  andAnswer:@"1This is an answer to the question above. This answer may also span multiple lines. Maybe even three or four lines long this answer can be. oh em gee. Lorem ipsum dolor sit amet."]];
-	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"2This is a question that may span multiple lines. Lorem ipsum dolor sit amet jklsdfljksdfjkldfsjklfsdljksdfjkldfsjklfsdjklfsdkjlsfdjlksfd"
-																	  andAnswer:@"2This is an answer to the question above."]];
-	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"3This is a question that may span multiple lines. Wah wah wah wah wah wah wah wah"
-																	  andAnswer:@"3This is an answer to the question above."]];
-	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"4This is a question that may span multiple lines."
-																	  andAnswer:@"4This is an answer to the question above."]];
-	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"5This is a question that may span multiple lines."
-																	  andAnswer:@"5This is an answer to the question above."]];
+	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"Are passwords being sent to a server?"
+																	  andAnswer:@"No. No data is ever sent to or from Passcode. All passwords are generated using code within the app itself. Password and other usage data are never sent outside of the app."]];
+	
+	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"Is Passcode available on other platforms?"
+																	  andAnswer:@"Yes! Passcode is available on several other platforms. Information on where to get these apps is available on *http://passcod.es*."]];
+	
+	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"After generating a passcode, how can it be retrieved again?"
+																	  andAnswer:@"Simply generate it again. Using the same domain and master password, this app will always generate the same output."]];
+	
+	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"Can the master password be found from the generated passcode?"
+																	  andAnswer:@"No. Passcode uses an algorithm that cannot be reversed. Generated passcodes can be used without the possibility of someone figuring out the master password."]];
+	
+	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"How is this different than 1Password?"
+																	  andAnswer:@"The 1Password app randomly generates passwords and stores them in a database. That's data that can be lost or stolen. If the database file is lost or becomes corrupted, access to all accounts can be gone forever. Or even worse, if that database is compromised, someone else may have access to your accounts.\nPasscode generates passcodes on the fly every time they're needed and are not stored. Passcode also works offline and doesn't require syncing between platforms."]];
+	
+	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"Is there a faster way to enter the domain field?"
+																	  andAnswer:@"Opening the app with a proper URL in the clipboard will automatically fill the domain field. A bookmarklet can also be added to your web browser to open Passcode with the domain of the current page. *[Include information on how to install javascript bookmarklet].*"]];
+	
+	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"How is this app free?"
+																	  andAnswer:@"Passcode is developed by *students* as part of a research program at *Rensselaer Polytechnic Institute*. The source code is *open-souce*. Students gain knowledge and experience, and users get free software. Everyone wins."]];
+	
+	[_questionsAndAnswers addObject:[MTZFrequentlyAskedQuestion faqWithQuestion:@"How can I thank you?"
+																	  andAnswer:@"You can help by kindly donating. Or, if you have software development experience, you can help by contributing to the development of this project for other platforms. Visit *http://passcod.es* for more information."]];
 	
 	_tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
 	[_tableView setAutoresizingMask:(UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth)];
