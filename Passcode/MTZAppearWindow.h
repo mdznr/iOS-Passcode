40a42ef685a582fef9700a7707e8a5d63f247073
Matt Zanchelli <matt@mdznr.com>
2014-04-20 19:15:46 -0400

    Documenting header of MTZAppearWindow.

--- Passcode/MTZAppearWindow.h
+++ Passcode/MTZAppearWindow.h
@@ -11,10 +11,16 @@
 
 @interface MTZAppearWindow : UIWindow
 
+///	The image to display in the view.
 @property (strong, nonatomic) UIImage *image;
+
+///	The text to display below the image.
 @property (strong, nonatomic) NSString *text;
+
+///	The size of the text.
 @property (nonatomic) CGFloat textSize;
 
+///	Display the view.
 - (void)display;
 
 @end
