026b1ce8910a65f7c43f8e0f7b164864192e7b56
Matt Zanchelli <matt@mdznr.com>
2014-04-21 10:48:27 -0400

    Added character method to NSString+Repeated. Cleaning up NSString+Repeated file.

--- Passcode/NSString+Repeated.m
+++ Passcode/NSString+Repeated.m
@@ -10,31 +10,55 @@
 
 @implementation NSString (Repeated)
 
-#warning There has to be more efficient ways to do this
-#warning Is it worth it to do it in C?
-#warning Can this be done better with NSString (without C)?
++ (NSString *)stringByRepeatingCharacter:(unichar)character numberOfTimes:(NSUInteger)numberOfTimes
+{
+	// Create a new C string of the specified length (+1 for null terminator).
+	unichar *newString = (unichar *) malloc(sizeof(unichar) * numberOfTimes+1);
+	
+	// Repeatedly append the character a numberOfTimes.
+	NSUInteger i;
+	for ( i=0 ; i<numberOfTimes; ++i ) {
+		newString[i] = character;
+	}
+	
+	// Terminate the string.
+	newString[i] = '\0';
+	
+	// Create the NSString version.
+	NSString *myNewString = [NSString stringWithCharacters:newString length:i];
+	
+	// The C string is no longer needed.
+	free(newString);
+	
+	return myNewString;
+}
 
 + (NSString *)stringByRepeatingString:(NSString *)string numberOfTimes:(NSUInteger)numberOfTimes
 {
-	NSString *ret = @"";
+	// Create a new NSString for creating the new string.
+	NSString *newString = [[NSString alloc] init];
+	
+	// Repeatedly append the string a numberOfTimes.
 	for ( NSUInteger i=0; i<numberOfTimes; ++i ) {
-		ret = [ret stringByAppendingString:[NSString stringWithFormat:@"%@", string]];
+		newString = [newString stringByAppendingString:[NSString stringWithFormat:@"%@", string]];
 	}
-	return ret;
+	
+	return newString;
 }
 
 - (NSString *)stringByInsertingString:(NSString *)string everyNumberOfCharacters:(NSUInteger)numberOfCharacters
 {
-	NSString *newString = @"";
-#warning How OK is it to use CGFloat for non-graphics things?
-	CGFloat numberOfChunks = 1;
+	// Create a new NSString for creating the new string.
+	NSString *newString = [[NSString alloc] init];
+	
+	// Repeatedly insert the string every numberOfCharacters.
 	for ( NSUInteger i=0; i<self.length; ++i ) {
-		if ( i>0 && i%4 == 0 ) {
+		if ( i % numberOfCharacters == 0 ) {
 			newString = [newString stringByAppendingString:string];
-			numberOfChunks++;
 		}
 		newString = [newString stringByAppendingFormat:@"%c", [self characterAtIndex:i]];
 	}
+	
 	return newString;
 }
 
