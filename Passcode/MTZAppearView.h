ccacb0484c303af0e066ff882879948705155834
Matt Zanchelli <matt@mdznr.com>
2014-04-21 11:13:43 -0400

    Documenting MTZAppearView header.

--- Passcode/MTZAppearView.h
+++ Passcode/MTZAppearView.h
@@ -11,10 +11,16 @@
 
 @interface MTZAppearView : UIView
 
+///	The image to display in the appear view.
 @property (strong, nonatomic) UIImage *image;
+
+///	The text to have in the appear view's label.
 @property (strong, nonatomic) NSString *text;
+
+///	The size of the text in the appear view's label.
 @property (nonatomic) CGFloat textSize;
 
+///	Display the appear view. It will automatically hide after a duration.
 - (void)display;
 
 @end
